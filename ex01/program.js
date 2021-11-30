bd2935ff405a1dc7e08d36ebcb479e26ff3970d5
admin <admin@iMac-ITA-02.local>
2021-11-30 10:14:55 +0100

    ex01

--- ex01/program.js
+++ ex01/program.js
@@ -1,5 +1,12 @@
 function firstDuplicateValue(array){
-     
+     for(let i = 0; i<array.length ; i++){
+         if(array[i]<=0){
+             return("le tableau contient des valeurs inférieur ou égal à 0");
+         }else if(array.indexOf(array[i], i+1) != -1){
+             return array[i];
+         }
+         
+     }
     }
     
     // Merci de ne pas effacer la ligne en dessous.
