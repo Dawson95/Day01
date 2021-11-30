9b83129ac480b2b50b96a34b094bcbbbbd36e6d5
admin <admin@iMac-ITA-02.local>
2021-11-30 17:17:17 +0100

    rush01

--- Rush01/program.js
+++ Rush01/program.js
@@ -1,6 +1,22 @@
 function validIPAdresses(string) {
-  
+
+//compter le nbr de chiffres
+if(string.length >=4 && string.length <=12){
+
 }
-    
+//diviser la string en 4 sequence avc 3 '.'
+//chaque séquence max 3 chiffres
+// verifier si chaque sequence  est compris entre 0 et 255 max
+//les séquence ne peuvent pas commencé par 0 et être suivi d'un autre int 
+//101 passe par contre
+
+//tester la séquence si la séquence est bonne compteur +1
+
+//si chaque conditions = true alors récup les séquences
+
+//si compteur 
+
+}
+    /
     // Merci de ne pas effacer la ligne en dessous.
     exports.validIPAddresses = validIPAddresses;
